8800aa9c6e1cceb83ed1b05e933767ee5a201364
Mustafa Çiçek <mustafa06cck@gmail.com>
2022-12-15 18:31:44 +0300

    app managers and app structure created.

---
+++ GameRatingApp/GameRatingApp/Core/Managers/Alert/AlertError.swift
@@ -0,0 +1,29 @@
+//
+//  AlertError.swift
+//  GameRatingApp
+//
+//  Created by Mustafa Çiçek on 15.12.2022.
+//
+
+import Foundation
+
+
+enum AlertError: Error {
+    case emptyInput
+    case wrongInput
+    case success
+}
+
+extension AlertError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .emptyInput:
+            return "Value cannot be blank. Please enter a value."
+        case .wrongInput:
+            return "Please enter a valid value."
+        case .success:
+            return "New employee was successfully added."
+        
+        }
+    }
+}
---
+++ GameRatingApp/GameRatingApp/Core/Managers/Alert/AlertManager.swift
@@ -0,0 +1,29 @@
+//
+//  AlertManager.swift
+//  GameRatingApp
+//
+//  Created by Mustafa Çiçek on 15.12.2022.
+//
+
+
+import UIKit
+
+
+protocol AlertShowable {
+    func showAlert(with error: AlertError?,localizeDescription: String?,title: String?)
+}
+
+
+final class AlertManager: AlertShowable {
+    static let shared: AlertManager = .init()
+    
+    func showAlert(with error: AlertError?, localizeDescription: String?, title: String?)  {
+        let alert = UIAlertController(title: title ?? "Opps!!", message: (error?.description ?? localizeDescription) ?? "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
+        DispatchQueue.main.async {
+            UIApplication.topViewController()?.present(alert, animated: true)
+        }
+    }
+}
